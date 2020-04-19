f2f7f578f16c54afa6f323e3c6209712bf8fe212
Ousmane! <ousmane.sylla@ynov.com>
2020-04-20 00:00:35 +0200

    images

--- javascripts/galeries/images.js
+++ javascripts/galeries/images.js
@@ -68,32 +68,6 @@
 
 
 
-    //Switch light/dark
-
-
-
-    $("#switch").on('click', function () {
-
-        if ($("body").hasClass("dark")) {
-
-            $("body").removeClass("dark");
-
-            $("#switch").removeClass("switched");
-
-        }
-
-        else {
-
-            $("body").addClass("dark");
-
-            $("#switch").addClass("switched");
-
-        }
-
-    });
-
-
-
 })(jQuery);
 
 
